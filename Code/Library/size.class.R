1b5e2c85ddaf7e5e30b9211703346bc178aaf9ca
McleodMJ <montana.mcleod@oregonstate.edu>
2020-12-11 12:10:33 -0800

    Include all sizes in size class option

--- Code/Library/size.class.R
+++ Code/Library/size.class.R
@@ -1,7 +1,11 @@
-##################################################################
+#########################################################################################
 # Size Class function 
 ### function to subset from main dataframe based on size and sex
-##################################################################
+
+#' data = datset to be filtered for size and sex 
+#' sex = 1: males // 2: females
+#' size = #juv: juveniles, sub: sub-adults, legal: legal-sized crabs, total: all crabs
+##########################################################################################
 
 size.class <-function(data, sex, size){
   if(size == "juv"){
@@ -13,5 +17,9 @@ size.class <-function(data, sex, size){
   if(size == "legal"){
     sized_data <- subset(data, CW  >= 160 & Sex == sex)
   }
+  if(size == "total"){
+    sized_data <- subset(data, Sex == sex)
+  }
+  
   return(sized_data)
 }
