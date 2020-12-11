e3dec21b1e62720499e617a858768fa57ae18cb2
McleodMJ <montana.mcleod@oregonstate.edu>
2020-12-11 13:00:14 -0800

    Update fxn to include Only males and a total males option

--- Code/Library/total.pots.crabs.R
+++ Code/Library/total.pots.crabs.R
@@ -3,32 +3,38 @@
 ### takes the dataframe in place and calculates:
 # 1) total number of pots per sample
 # 2) total number of crabs per sample 
+
+#' data = full dataset
+#' size = sub: only sub-adult males // total: all males 
 ##################################################
 
-total.pots.crabs <- function(data){
-  #When there is a different sample date (i.e. sample), records total pots used and number of crabs collected 
-  data$Total.pots <- rep(NA,nrow(data))
-  data$Total.crabs <- rep(NA,nrow(data))
-  
-  j=1
-  for(i in 2:nrow(data)){
-    if(data$Date[i] !=  data$Date[i-1])
-    { data$Total.pots[j:i] <- data$Pot[i-1];
-    data$Total.crabs[j:i] <- (i-j);
-    j=i}
+total.pots.crabs <- function(data, size){
+
+# for no size limitations
+  if(size == "total"){
+    data.summary <- data %>% group_by(Date) %>% 
+    summarise(Total.crabs = n(), Total.pots = max(Pot))
+    # append the table to the main data table
+    data <- inner_join(data, data.summary, by = 'Date')
     
-    if(i == nrow(data)) 
-    {data$Total.pots[j:i] <- data$Pot[i];
-    data$Total.crabs[j:i] <- i-j +1}
+    #now account for JUST the sub-adult males
+    data <- size.class(data, 1, "total")
   }
+  
+  if(size == "sub"){
+    data.summary <- data %>% group_by(Date) %>% 
+      summarise(Total.pots = max(Pot))
+    # append the table to the main data table
+    data <- inner_join(data, data.summary, by = 'Date')
+    
+    #now account for JUST the sub-adult males
+    Sub_males <- size.class(data, 1, "sub")
+  
+  data.sub <- Sub_males %>% group_by(Date) %>% 
+    summarise(Total.crabs = n())
+    # append the table to the main data table
+    data <- inner_join(Sub_males, data.sub, by = 'Date')
+  }
+  
   return(data)
 }
-
- 
- ## attempt at tidyverse 
- ### need to fix it so that all the columns in select are showing
- #data <- data %>% 
-  # select(Date, Month, Year, Sex, CW, Pot, Soak.Time.hr) %>% 
-   #group_by(Date) %>% 
-   #summarise(Total.Pots = max(Pot)) %>%
-   #arrange(Date, Month, Year, Sex, CW, Pot, Soak.Time.hr, Total.Pots)
