3492f1bad24c5ee7d48b195cf6b202cbd5cde151
McleodMJ <montana.mcleod@oregonstate.edu>
2022-01-11 15:59:43 -0800

    subset by dates

--- Code/Library/est.mort.InOut.R
+++ Code/Library/est.mort.InOut.R
@@ -10,21 +10,21 @@
 
 #' data = data of all male crab sizes
 #' y = year (numeric)
-#' a = change the format of the answer? not sure why..
+#' a = change the format of the answer - includes proportion of cohort moving to next size class which is just 1
 #########################################################################################################
 
 # Function for proportion of pop that molts from April --> September
-surv.molt <- function(data, y, a){
-  Ap_Sept_Data <- subset(data, Date == "2014-04-07" | Date == "2014-09-17"| Date == "2020-04-03" |Date == "2020-09-01" |Date == "2020-09-15")
+est.mort.InOut <- function(data, y, a){
+  data <- subset(data, Date == "2014-04-07" | Date == "2014-09-17"| Date == "2020-04-03" |Date == "2020-09-01" |Date == "2020-09-15" | Date == "2021-04-16" |Date == "2021-08-31")
   
   #create pre and post df for each size class 
-  Pre_molt_juv <- subset(Ap_Sept_Data, CW <= 139 & Month == "April" & Year == y) #juveniles moving into sub-adults
+  Pre_molt_juv <- subset(data, CW <= 139 & Month == "April" & Year == y) #juveniles moving into sub-adults
   
   # sub-adults have to be spilt into two b/c of zhang LR calc
-  Pre_molt_sub1 <- subset(Ap_Sept_Data, CW %in% c(140:149) & Month == "April"& Year == y)
-  Pre_molt_sub2 <- subset(Ap_Sept_Data, CW %in% c(150:159) & Month == "April"& Year == y)
+  Pre_molt_sub1 <- subset(data, CW %in% c(140:149) & Month == "April"& Year == y)
+  Pre_molt_sub2 <- subset(data, CW %in% c(150:159) & Month == "April"& Year == y)
   
-  Post_molt_sub <- subset(Ap_Sept_Data, CW %in% c(140:159) & Month == "September" & Year == y)
+  Post_molt_sub <- subset(data, CW %in% c(140:159) & Month == "September" |Month == "August" & Year == y)
   
   molt.in = nrow(Pre_molt_juv) * mean(Pre_molt_juv$Molt.prob) 
   molt.out = (nrow(Pre_molt_sub1) * mean(Pre_molt_sub1$Molt.prob)) + (nrow(Pre_molt_sub2)* mean(Pre_molt_sub2$Molt.prob)) 
@@ -34,5 +34,3 @@ surv.molt <- function(data, y, a){
 
 # We have a lot more datapoints for september of 2020 which is why we have such a large 2020 value
 #table(Ap_Sept_Data$Date) 
-
-## change the fxn name to est.mort.in.out()
